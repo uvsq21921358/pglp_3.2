51a5826d9a183b24f0d43aaea15784497bc62f58
Utilisateur <user@debian.arcahe.ovh>
2020-03-03 17:50:42 +0100

    Modification

---
+++ src/main/java/uvsq21921358/OCP/App.java
@@ -0,0 +1,13 @@
+package uvsq21921358.OCP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
